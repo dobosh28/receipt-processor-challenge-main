5f8102b4c9ecd6db27b0fbe8b937202a964ee64c
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-10-05 03:53:06 -0400

    make logical edits. Everything is functional

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { parse } = require("uuid");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
@@ -13,32 +12,32 @@ app.get("/", (req, res) => {
   res.send("Receipt Processor Service");
 });
 
+// Middleware to ensure content type is application/json
+function ensureJsonContentType(req, res, next) {
+  const contentType = req.headers["content-type"];
+  if (contentType && contentType.includes("application/json")) {
+    next();
+  } else {
+    res
+      .status(400)
+      .json({ error: "Expected application/json content-type in header." });
+  }
+}
+
 // POST endpoint for /receipts/process
-app.post("/receipts/process", (req, res) => {
+app.post("/receipts/process", ensureJsonContentType, (req, res) => {
   const receipt = req.body;
 
   // Validate the receipt
-  if (
-    !receipt.retailer ||
-    !receipt.purchaseDate ||
-    !receipt.items ||
-    !receipt.total
-  ) {
-    res.status(400).json({ error: "Invalid receipt data" });
+  const validationResult = isValidReceipt(receipt);
+  if (validationResult !== true) {
+    res.status(400).json({ error: validationResult });
     return;
   }
 
-  // Generate a unique ID for the receipt
   const id = uuidv4();
-
-  // Calculate the points
   const points = calculatePoints(receipt);
-
-  // Store the receipt and its points
-  receipts[id] = {
-    data: receipt,
-    points: points,
-  };
+  receipts[id] = { data: receipt, points: points };
 
   res.json({ id: id });
 });
@@ -46,46 +45,74 @@ app.post("/receipts/process", (req, res) => {
 // GET endpoint for /receipts/{id}/points
 app.get("/receipts/:id/points", (req, res) => {
   const id = req.params.id;
-
-  // Validate the ID
   if (!receipts[id]) {
     return res.status(404).json({ error: "No receipt found for that id" });
   }
-
   res.json({ points: receipts[id].points });
 });
+
+// Validation function to check if a receipt is valid
+function isValidReceipt(receipt) {
+  if (!receipt.retailer || !/^\S(.*\S)?$/g.test(receipt.retailer))
+    return "Invalid retailer";
+  if (
+    !receipt.purchaseDate ||
+    !/^\d{4}-\d{2}-\d{2}$/.test(receipt.purchaseDate)
+  )
+    return "Invalid purchase date";
+  if (!receipt.purchaseTime || !/^\d{2}:\d{2}$/.test(receipt.purchaseTime))
+    return "Invalid purchase time";
+  if (!Array.isArray(receipt.items) || receipt.items.length < 1)
+    return "Invalid items";
+  for (let item of receipt.items) {
+    if (!item.shortDescription || !/^[\w\s\-]+$/.test(item.shortDescription))
+      return "Invalid item description";
+    if (!item.price || !/^\d+\.\d{2}$/.test(item.price))
+      return "Invalid item price";
+  }
+  if (!receipt.total || !/^\d+\.\d{2}$/.test(receipt.total))
+    return "Invalid total";
+
+  return true;
+}
 
 function calculatePoints(receipt) {
   let points = 0;
 
   // One point for every alphanumeric character in the retailer name
   points += receipt.retailer.replace(/[^0-9a-z]/gi, "").length;
+  console.log("Points after retailer:", points);
 
   // 50 points if the total is a round total amount with no cents
   if (parseFloat(receipt.total) === Math.floor(parseFloat(receipt.total))) {
     points += 50;
   }
+  console.log("Points after round total check:", points);
 
   // 25 points if the total is a multiple of 0.25
-  if (parseFloat(receipt.total) % 0.25 === 0) {
+  if ((parseFloat(receipt.total) * 100) % 25 === 0) {
     points += 25;
   }
+  console.log("Points after 0.25 multiple check:", points);
 
   // 5 points for every two items on the receipt
   points += Math.floor(receipt.items.length / 2) * 5;
+  console.log("Points after items count:", points);
 
-  // If the trimmed length of the item description is a multiple of 3, multiply the price by `0.2` and round up to the nearest integer. The result is the number of points earned
+  // If the trimmed length of the item description is a multiple of 3
   receipt.items.forEach((item) => {
     if (item.shortDescription.trim().length % 3 === 0) {
       points += Math.ceil(parseFloat(item.price) * 0.2);
     }
   });
+  console.log("Points after item description check:", points);
 
   // 6 points if the day in the purchase date is odd
-  const day = new Date(receipt.purchaseDate).getDate();
+  const day = new Date(receipt.purchaseDate).getUTCDate();
   if (day % 2 === 1) {
     points += 6;
   }
+  console.log("Points after date check:", points);
 
   // 10 points if the the time of purchase is after 2:00pm and before 4:00pm
   const purchaseTime = receipt.purchaseTime.split(":");
@@ -93,6 +120,7 @@ function calculatePoints(receipt) {
   if (hours >= 14 && hours < 16) {
     points += 10;
   }
+  console.log("Points after time check:", points);
 
   return points;
 }
